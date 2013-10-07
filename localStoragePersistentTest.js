b1eef6d5f90dc3f45c8c9d3b67e7adfa0cbefc4e
Ari Summer <ari@Aris-MacBook-Pro-3.local>
2013-10-07 08:15:07 -0700

    persistent partly working

--- localStoragePersistentTest.js
+++ localStoragePersistentTest.js
@@ -1,58 +1,44 @@
 //Ari Summer
 //Testing HTML5 permanent local storage in chrome
 
-/*
-navigator.webkitPersistentStorage.requestQuota(
-                    5 * 1024 * 1024,
-                    function (grantedBytes) {
-                        // we request a handle to the file system
-                        window.webkitRequestFileSystem(
-                            PERSISTENT,
-                            grantedBytes,
-                            // the fs arg in this callback is our filesystem ref
-                            function (fs) {
-                                self.fs = fs;
-                                self.loadExistingPrefs(function(contents) {
-                                    if(contents.length) {
-                                        console.log("We already have content we'll use");
-                                        self.emit("prefs.exist", contents);
-                                    } else {
-                                        console.log("We need to load seed data....");
-                                        self.loadSeedData(function() {
-                                            self.loadExistingPrefs(); 
-                                        });   
-                                    }
-                                });
-                            },
-                            errorHandler);
-                    },
-                    errorHandler
-                );
-*/
+function onError () { console.log ('Error : ', arguments); }
 
-
+function requestFS(grantedBytes) {
+	window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
+		console.log ('fs: ', arguments); // I see this on Chrome 27 in Ubuntu
+		console.log("Granted Bytes: " + grantedBytes);
+		console.log("**********************************");
+	}, onError);
+}
 
 function getGranted(){
-	navigator.webkitPersistentStorage.requestQuota(1024*1024,
-		function(grantedBytes) {
-			console.log("==========================");
-			console.log("PERSISENT STORAGE");
-			console.log("==========================");
+	navigator.webkitPersistentStorage.requestQuota (1024*1024*1024, function(grantedBytes) {
+		console.log("==================================");
+		console.log("PERSISENT STORAGE");
+		console.log("==================================");
 
-			window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+		console.log("**********************************");
+		console.log ('requestQuota: ', arguments);
+		
+		requestFS(grantedBytes);
 
-			console.log("**********************************");
-			console.log("Granted bytes: " + grantedBytes);
-	 		console.log("**********************************");
-		}, function(e) {
-	  		console.log('Error', e); 
-	});
+	}, onError);
 }
 
 function byteCount(s) {
     return encodeURI(s).split(/%..|./).length - 1;
 }
 
+function clearStorage(){
+	localStorage.clear();
+}
+
+function printStorage(){
+	for(key in localStorage) {
+		console.log(key);
+		console.log(localStorage[key]);
+	}
+}
 
 window.onload = function(){
 
@@ -69,13 +55,10 @@ window.onload = function(){
 			console.log("Size of data: " + byteCount(ev2.target.result) + " bytes.");
 			
 			localStorage["file"] = ev2.target.result;
-
+			localStorage.setItem("file", ev2.target.result);
 
 			console.log("Pull file data from local storage:");
 			console.log(localStorage["file"]);
-
-			getGranted();
-
 		};
 
 		//Start loading image
